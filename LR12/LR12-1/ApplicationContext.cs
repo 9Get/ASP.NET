21ddedf38874406305ebc649055f701df5d77e93
Stas <100081623+9Get@users.noreply.github.com>
2024-03-28 20:43:05 +0200

    LR12-1

---
+++ LR12/LR12-1/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR12_1
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=lr12-1.db");
+        }
+    }
+}
---
+++ LR12/LR12-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Linq;
+
+namespace LR12_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                User[] usersToDb = new User[] {
+                    new User { Name = "Ivan", Surname = "Petrov", Age = 35 },
+                    new User { Name = "Anna", Surname = "Smirnova", Age = 28 },
+                    new User { Name = "Mikhail", Surname = "Kozlov", Age = 42 }
+                };
+
+                for (int i = 0; i < usersToDb.Length; i++)
+                {
+                    db.Users.Add(usersToDb[i]);
+                }
+                
+                db.SaveChanges();
+
+                Console.WriteLine("Added successfully");
+
+                var users = db.Users.ToList();
+                Console.WriteLine("List of users:");
+
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"{user.Id}.{user.Name} {user.Surname} - {user.Age}");
+                }
+            }
+        }
+    }
+}
